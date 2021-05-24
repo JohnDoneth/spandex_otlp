c3fe9ad23891a6aa5fa27dc0c43d88d92e53a995
JohnDoneth <doneth7@gmail.com>
2021-05-23 21:59:09 -0400

    Add conversion of error fields

--- test/spandex_otlp/conversion_test.exs
+++ test/spandex_otlp/conversion_test.exs
@@ -85,5 +85,96 @@ defmodule SpandexOTLP.ConversionTest do
     test "result can be protobuf encoded", %{converted_span: converted_span} do
       Span.encode(converted_span)
     end
+
+    test "converts exceptions to errors" do
+      {:current_stacktrace, stacktrace} = Process.info(self(), :current_stacktrace)
+
+      converted_span = Conversion.convert_span(span_factory(
+        error: [
+          exception: ArgumentError.exception("foo"),
+          stacktrace: stacktrace
+        ]
+      ))
+
+      assert converted_span.status.code == :STATUS_CODE_ERROR
+      assert converted_span.status.message == "foo"
+
+      assert Enum.member?(
+               converted_span.attributes,
+               %KeyValue{
+                 key: "exception.message",
+                 value: %AnyValue{
+                   value: {:string_value, "foo"}
+                 }
+               }
+             )
+
+      assert Enum.member?(
+              converted_span.attributes,
+              %KeyValue{
+                key: "exception.type",
+                value: %AnyValue{
+                  value: {:string_value, "ArgumentError"}
+                }
+              }
+            )
+
+      assert Enum.member?(
+              converted_span.attributes,
+              %KeyValue{
+                key: "exception.stacktrace",
+                value: %AnyValue{
+                  value: {:string_value, Exception.format_stacktrace(stacktrace)}
+                }
+              }
+            )
+    end
+
+    test "converts message only errors to errors" do
+      {:current_stacktrace, stacktrace} = Process.info(self(), :current_stacktrace)
+
+      converted_span = Conversion.convert_span(span_factory(
+        error: [
+          message: "something bad happened"
+        ]
+      ))
+
+      assert converted_span.status.code == :STATUS_CODE_ERROR
+      assert converted_span.status.message == "something bad happened"
+    end
+
+    test "converts message with error? flag" do
+      {:current_stacktrace, stacktrace} = Process.info(self(), :current_stacktrace)
+
+      converted_span = Conversion.convert_span(span_factory(
+        error: [
+          error?: true
+        ]
+      ))
+
+      assert converted_span.status.code == :STATUS_CODE_ERROR
+      assert converted_span.status.message == nil
+    end
+  end
+
+  defp span_factory(opts) do
+    fields = %{
+      id: SpandexOTLP.Adapter.span_id(),
+      trace_id: SpandexOTLP.Adapter.trace_id(),
+      private: [],
+      services: [],
+      sql_query: [
+        query: "select 1",
+        rows: 1,
+        db: "my-db"
+      ],
+      tags: [
+        "test-tag": "42"
+      ]
+    }
+
+    fields = Map.merge(fields, Enum.into(opts, %{}))
+
+    struct(Spandex.Span, fields)
   end
 end
